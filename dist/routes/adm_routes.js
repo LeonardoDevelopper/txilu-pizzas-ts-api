77164fc1825e4fc29e34405625e965fa08c751bc
LeonardoDevelopper <113784384+LeonardoDevelopper@users.noreply.github.com>
2023-12-20 09:22:13 +0000

    models created

--- dist/routes/adm_routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const routes = express.Router();
-routes.get('/', (req, res) => {
-    res.json({ Message: "Hello Word" });
-});
-module.exports = routes;
